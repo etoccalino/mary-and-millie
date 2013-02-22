3b81df036f50bdbc29c56c548746fee7cc5a6974
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-02-21 23:05:44 -0300

    config for the base templates.

--- mnm/settings.py
+++ mnm/settings.py
@@ -1,5 +1,6 @@
 # Django settings for mnm project.
 
+import os.path
 from local_settings import PROJECT_ROOT
 
 DEBUG = True
@@ -112,6 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT,'templates'),
 )
 
 INSTALLED_APPS = (
