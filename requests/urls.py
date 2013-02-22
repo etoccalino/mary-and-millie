b2d39833ac9a43d25e06eda8d831acd53fdf6ad1
Elvio Toccalino <etoccalino@creativa77.com.ar>
2013-02-22 14:10:33 -0300

    fixed bad routes in requests app.

--- requests/urls.py
+++ requests/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.requests, name="requests"),
-    url(r'^(?P<request_pk>\w+)/$', views.request, name="request"),
     url(r'^new/$', views.new_request, name="new_request"),
+    url(r'^(?P<request_pk>\w+)/$', views.request, name="request"),
 )
