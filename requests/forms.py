7de7a742abafaace3ad026b5f90f67a47017b84e
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-02-23 15:01:47 -0300

    crispy forms code, no templates yet.

--- requests/forms.py
+++ requests/forms.py
@@ -1,21 +1,38 @@
-from django.forms.models import inlineformset_factory
-from django import forms
+import django.forms
+from crispy_forms.helper import FormHelper
+from django.forms.formsets import formset_factory
 import models
 
 
+class ItemRequestForm(django.forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        super(ItemRequestForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = models.ItemRequest
+        fields = ('item', 'quantity')
+
+
 MAX_ITEMS = 3
-ItemRequestFormSet = inlineformset_factory(models.Request, models.ItemRequest,
-                                           can_delete=False, extra=MAX_ITEMS)
+ItemRequestFormSet = formset_factory(ItemRequestForm, extra=MAX_ITEMS)
 
 
-class RequestForm(forms.ModelForm):
+class RequestForm(django.forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        super(RequestForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = models.Request
         fields = ('location',)
 
 
-class AssociateRequestForm(forms.Form):
-    bin = forms.ModelChoiceField(
+class AssociateRequestForm(django.forms.Form):
+    bin = django.forms.ModelChoiceField(
         queryset=models.Bin.objects.filter(requested=False),
         empty_label="(select a bin)")
