ed0371f72fc7dd12f3a1a0f6e10ac0cf136edba5
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-02-24 16:58:00 -0300

    implemented crude DOM handling to reflect new requests in realtime.

--- requests/static/js/requests.js
+++ requests/static/js/requests.js
@@ -14,12 +14,17 @@ var Requests = {
 
   // Create a new row for a requests table, using data comming from the server.
   createRequestRow: function (requestData) {
-    console.log('createRequestRow() called.');
+    console.log('createRequestRow() called. with requestData: ' + requestData);
+    var obj = JSON.parse(requestData)
+      , row = '<tr><td>' + '(who)' + '</td><td>' + obj.description + '</td></tr>';
+    console.log('new row text is: ' + row);
+    return $(row)
   },
 
-  // Insert a request row into a particular request table (identified by the mapping).
-  insertRequestRow: function (requestRow, requestTable) {
-    console.log('insertRequestRow() called.');
+  // Insert a request row (a jQuery object) into a particular request table (identified by ID).
+  insertRequestRow: function (requestRow, requestTableID) {
+    console.log('inserting before "' + requestTableID + ' tr:first".');
+    $(requestTableID + ' tr:first').before(requestRow);
   },
 
   // --- Socket to handle requests events ---
